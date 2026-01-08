38e0ceea485dc9f3afdbb775bb5597aa74e0053d
AL <404notfoundcybersec@protonmail.com>
2026-01-08 13:34:05 -0500

    Update whois_scan.py

--- modules/whois_scan.py
+++ modules/whois_scan.py
@@ -1,1 +1,21 @@
+import whois
+from datetime import datetime
 
+def whois_check(domain):
+    try:
+        w = whois.whois(domain)
+        c = w.creation_date
+        if isinstance(c, list):
+            c = c[0]
+
+        age = (datetime.now() - c).days
+
+        if age < 180:
+            return 30, f"Domain sangat baru ({age} hari)"
+        elif age < 365:
+            return 15, f"Domain baru ({age} hari)"
+
+        return 0, f"Domain lama ({age} hari)"
+
+    except:
+        return 20, "WHOIS disembunyikan / gagal"
