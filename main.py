5de0892dbde019a0b73f46f09656b23449524a11
AL <404notfoundcybersec@protonmail.com>
2026-01-08 13:36:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from core.banner import banner
+from core.engine import run_engine
+from colorama import Fore, init
 
+init(autoreset=True)
+banner()
+
+url = input(Fore.CYAN + "[+] Target URL: ")
+
+score, status, color, report = run_engine(url)
+
+print(Fore.WHITE + "\n========== FULL SCAN RESULT ==========")
+print(f"Target     : {url}")
+print(f"Risk Score : {score}\n")
+
+for r in report:
+    print(f" - {r}")
+
+if color == "GREEN":
+    print(Fore.GREEN + f"\nVERDICT: {status}")
+elif color == "YELLOW":
+    print(Fore.YELLOW + f"\nVERDICT: {status}")
+else:
+    print(Fore.RED + f"\nVERDICT: {status}")
+
+print(Fore.RED + "\n[ WEB THREAT INTELLIGENCE | 404 Not Found CyberSec ]")
