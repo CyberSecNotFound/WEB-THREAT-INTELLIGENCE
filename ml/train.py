944e26782929e9275964c478f8c4a80ab98eac49
AL <404notfoundcybersec@protonmail.com>
2026-01-08 13:35:32 -0500

    Update train.py

--- ml/train.py
+++ ml/train.py
@@ -1,1 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+import joblib
 
+X = [
+    [120, 5, 80],
+    [60, 1, 10],
+    [150, 7, 95],
+    [50, 1, 5]
+]
+y = [1, 0, 1, 0]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "ml/model.pkl")
+print("ML Model trained")
