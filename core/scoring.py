89cc520865cd2fc35679829caf1ce9b29e6ac7f0
AL <404notfoundcybersec@protonmail.com>
2026-01-08 13:32:14 -0500

    Update scoring.py

--- core/scoring.py
+++ core/scoring.py
@@ -1,1 +1,7 @@
-
+def verdict(score):
+    if score < 30:
+        return "AMAN", "GREEN"
+    elif score < 70:
+        return "MENCURIGAKAN", "YELLOW"
+    else:
+        return "BERBAHAYA", "RED"
