57c812602d5d6f536510959ab6d1e6846d660dd8
AL <404notfoundcybersec@protonmail.com>
2026-01-08 13:31:37 -0500

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,1 +1,15 @@
+from colorama import Fore
 
+def banner():
+    print(Fore.RED + r"""
+ ██╗    ██╗████████╗██╗
+ ██║    ██║╚══██╔══╝██║
+ ██║ █╗ ██║   ██║   ██║
+ ██║███╗██║   ██║   ██║
+ ╚███╔███╔╝   ██║   ██║
+  ╚══╝╚══╝    ╚═╝   ╚═╝
+
+ WEB THREAT INTELLIGENCE
+ Advanced Security Scanner
+ by 404 Not Found CyberSec
+""")
